5945a84bd6c0867a5cc89486d67ab019530d6afe
Connor Hansen <chansen13@georgefox.edu>
2021-09-07 10:04:40 -0700

    add open weather api project.

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
     $('#location').val("");
 
     let request = new XMLHttpRequest();
-    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`;
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}&lang=en`;
 
     request.onreadystatechange = function() {
       if (this.readyState === 4 && this.status === 200) {
@@ -24,6 +24,8 @@ $(document).ready(function() {
     function getElements(response) {
       $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
       $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
+      $('.showFarenheit').text(`The temperature in Farenheit is ${((response.main.temp - 273.15) * 9/5 + 32).toFixed(1)} degrees.`);
+      $('.showDescription').text(`Description: ${response.weather[0].description}`);
     }
   });
 });
